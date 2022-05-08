9dd9b91e19b37fd92d8a6645cb628bb17a41d354
Deby1997 <debysarker159@gmail.com>
2022-05-08 18:19:33 +0600

    question added

--- src/Components/Blog/Blog.js
+++ src/Components/Blog/Blog.js
@@ -3,7 +3,13 @@ import React from 'react';
 const Blog = () => {
   return (
     <div>
-      <h6>Queation:</h6>
+      <h6>Question: Difference between javascript and nodejs?</h6>
+      <p>Answer:</p>
+      <h6>Question:When should you use nodejs and when should you use mongodb?</h6>
+      <p>Answer:</p>
+      <h6>Question:Differences between sql and nosql databases?</h6>
+      <p>Answer:</p>
+      <h6>Question:What is the purpose of jwt and how does it work?</h6>
       <p>Answer:</p>
     </div>
   );
